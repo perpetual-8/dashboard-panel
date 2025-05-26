8df020318409a1903a3167832c3e2651be2cc5be
Hamza Ahmad Bhutta <hamza.ahmad.topdot@gmail.com>
2025-05-26 19:58:15 +0500

    layout fix of sidebar

--- src/components/macroUI/SideBar.vue
+++ src/components/macroUI/SideBar.vue
@@ -1,22 +1,23 @@
 <script setup>
 const navItems = [
-  { name: 'Dashboard', route: '/revenue', icon: 'bi bi-speedometer2' },
-  { name: 'Products', route: '/inventory', icon: 'bi bi-box' },
-  { name: 'Add Product', route: '/register', icon: 'bi bi-plus-circle' }
-]
+  { name: "Revenue Analysis", route: "/revenue", icon: "bi bi-speedometer2" },
+  { name: "Inventory Management", route: "/inventory", icon: "bi bi-box" },
+  { name: "Register Product", route: "/register", icon: "bi bi-plus-circle" },
+];
 </script>
 
-
 <template>
-  <div class="d-flex flex-column p-3 bg-light min-vh-100" style="width: 240px;">
+  <div class="d-flex flex-column p-3 bg-light min-vh-100" style="width: 240px">
     <h4 class="mb-4">Commerce Admin</h4>
     <ul class="nav nav-pills flex-column">
       <li class="nav-item" v-for="item in navItems" :key="item.name">
         <router-link class="nav-link" :to="item.route">
-          <i :class="item.icon" class="me-2"></i>{{ item.name }}
+          <div class="row align-items-center">
+            <div class="col-2"><i :class="item.icon" class="me-2"></i></div>
+            <div class="col-10">{{ item.name }}</div>
+          </div>
         </router-link>
       </li>
     </ul>
   </div>
 </template>
-
